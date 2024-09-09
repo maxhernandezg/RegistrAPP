e3e26de448dd233bd3df60e63eb9e8506042489d
Maximiliano Hernández <max.hernandezg@duocuc.cl>
2024-09-08 22:54:39 -0300

    blessed

--- RegistrAPP/src/app/home/home.page.ts
+++ RegistrAPP/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  usuario: string = ''; // Declarar la variable para almacenar el nombre del usuario
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public alertController: AlertController, private activeroute: ActivatedRoute, private router: Router) {
+    // Suscribirse a los parámetros de la ruta para obtener el estado
+    this.activeroute.queryParams.subscribe(params => {
+      const navigation = this.router.getCurrentNavigation();
+      
+      // Verificar si el estado y el campo 'user' están presentes
+      if (navigation && navigation.extras && navigation.extras.state && navigation.extras.state['user']) {
+        this.usuario = navigation.extras.state['user']; // Asignar el nombre del usuario
+      }
+    });
   }
 
+  ngOnInit() {}
 }
