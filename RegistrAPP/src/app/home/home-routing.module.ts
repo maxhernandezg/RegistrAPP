04be21884c38c3c4088f7c2be72991343fa7f82c
jotafc <133885139+joaferrada@users.noreply.github.com>
2024-10-01 09:52:22 -0300

    commit

--- RegistrAPP/src/app/home/home-routing.module.ts
+++ RegistrAPP/src/app/home/home-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { AsistenciaComponent } from '../asistencia/asistencia.component';
 import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomePage
+    component: HomePage,
+    children:[
+    {
+      path:'asistencia',
+      component:AsistenciaComponent
+    }
+    ]
   }
 ];
 
--- RegistrAPP/src/app/home/home.page.ts
+++ RegistrAPP/src/app/home/home.page.ts
@@ -1,26 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
-  usuario: string = ''; // Declarar la variable para almacenar el nombre del usuario
-
-  constructor(public alertController: AlertController, private activeroute: ActivatedRoute, private router: Router) {
-    // Suscribirse a los parámetros de la ruta para obtener el estado
-    this.activeroute.queryParams.subscribe(params => {
-      const navigation = this.router.getCurrentNavigation();
-      
-      // Verificar si el estado y el campo 'user' están presentes
-      if (navigation && navigation.extras && navigation.extras.state && navigation.extras.state['user']) {
-        this.usuario = navigation.extras.state['user']; // Asignar el nombre del usuario
-      }
-    });
+export class HomePage {
+  
+  constructor(private authService:AuthService, private router: Router) {
+    this.router.navigate(['home/perfil'])
   }
-
-  ngOnInit() {}
+  segmentChanged($event: any){
+    console.log($event.detail.value);
+    let direction=$event.detail.value
+    this.router.navigate(['home/'+direction])
+  }
+  logOut(){
+    this.authService.removeToken();
+    this.router.navigate(['/login']);
+  }
 }
